a65eec36fbb725da6e4e438df66affddc0f63028
berberman <1793913507@qq.com>
2018-08-14 15:06:26 +0800

    为挂起状态机引擎添加异常捕获

--- src/main/kotlin/org/mechdancer/statemachine/core/engine/coroutine/SuspendedEngine.kt
+++ src/main/kotlin/org/mechdancer/statemachine/core/engine/coroutine/SuspendedEngine.kt
@@ -2,7 +2,6 @@ package org.mechdancer.statemachine.core.engine.coroutine
 
 import org.mechdancer.statemachine.core.Ending
 import org.mechdancer.statemachine.core.IStateHandler
-import org.mechdancer.statemachine.core.engine.coroutine.ISuspendedEngine
 import kotlin.coroutines.experimental.suspendCoroutine
 
 class SuspendedEngine(origin: IStateHandler) : ISuspendedEngine {
@@ -10,8 +9,12 @@ class SuspendedEngine(origin: IStateHandler) : ISuspendedEngine {
 	private var current = origin
 
 	override suspend fun run(): Boolean = suspendCoroutine {
-		current = current.run()
-		it.resume(current == Ending.Destination)
+		try {
+			current = current.run()
+			it.resume(current == Ending.Destination)
+		} catch (e: Exception) {
+			it.resumeWithException(e)
+		}
 	}
 
 }
